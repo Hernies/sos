0caef0eab5251fd27c7e04435979dafa1e6e48ff
Hernies <hernancalvoaguiar.2@gmail.com>
2022-04-15 03:48:54 +0200

    TODO func bodies

--- geoetsiinf/src/main/java/com/sos/UsuarioResource.java
+++ geoetsiinf/src/main/java/com/sos/UsuarioResource.java
@@ -1,6 +1,7 @@
 package com.sos;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.DELETE;
@@ -67,6 +68,54 @@ public class UsuarioResource {
         return "usuario borrado correctamente";
     }
 
+
+    @POST
+    @Path("/{usuario_id}/tesoros_añadidos")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String publicarTesoro(@PathParam("usuario_id") String id){
+
+        //TODO MÉTODO
+
+        return "Se ha añadido el texto correctamente";
+    }
+
+    @GET
+    @Path("/{usuario_id}/tesoros_añadidos")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Tesoro> getTesorosCreadosUsuario(@PathParam("usuario_id") String id, @QueryParam("string") String string, @QueryParam("fecha") Date date, 
+    @QueryParam("desplazamiento") int desplazamiento, @QueryParam("terreno") String terreno, @QueryParam("dificultad") int dificultad){
+        
+        //TODO QUERYS 
+        //TODO METODO
+
+        //simplemente para que no haya 500 errores
+        List<Tesoro> tesoros = new ArrayList<Tesoro>();
+            return tesoros;
+    }
+
+    @POST
+    @Path("/{usuario_id}/tesoros_descubiertos")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String encontrarTesoro(@PathParam("usuario_id") String id){
+
+        //TODO MÉTODO
+
+        return "Se ha añadido el texto correctamente";
+    }
+
+    @GET
+    @Path("/{usuario_id}/tesoros_descubiertos")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Tesoro> getTesorosEncontradoUsuario(@PathParam("usuario_id") String id, @QueryParam("string") String string, @QueryParam("fecha") Date date, 
+    @QueryParam("desplazamiento") int desplazamiento, @QueryParam("terreno") String terreno, @QueryParam("dificultad") int dificultad){
+        
+        //TODO QUERYS 
+        //TODO METODO
+
+        //simplemente para que no haya 500 errores
+        List<Tesoro> tesoros = new ArrayList<Tesoro>();
+            return tesoros;
+    }
 
 
 }
